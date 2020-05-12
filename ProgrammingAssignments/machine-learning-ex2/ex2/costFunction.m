7b2daad59308a714706d393d84b9f5b9eac0720a
flizzer <briandavis1977@gmail.com>
2020-05-11 22:29:49 -0400

    adding work on non-regularized version of cost function for LogisticRegression exercise

--- ProgrammingAssignments/machine-learning-ex2/ex2/costFunction.m
+++ ProgrammingAssignments/machine-learning-ex2/ex2/costFunction.m
@@ -5,11 +5,11 @@ function [J, grad] = costFunction(theta, X, y)
 %   w.r.t. to the parameters.
 
 % Initialize some useful values
-m = length(y); % number of training examples
+   m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+   J = 0;
+   grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -21,7 +21,7 @@ grad = zeros(size(theta));
 %
 
 
-
+   J = ((sum(-y) * log(sigmoid(theta))) - (sum(1-y) * log(1-sigmoid(theta))))/m;
 
 
 
